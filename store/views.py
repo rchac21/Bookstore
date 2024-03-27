9a4a13684240d7b40f57cb7e5637a810432ff881
rchac21 <chachkhianirati7@gmail.com>
2024-03-27 16:37:37 +0400

    created viewsets and serializers - created viewsets for loggin - created viewsets for books and author models - defined permissions for models - created serializers

--- store/views.py
+++ store/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Author,Book
+from .serializers import AuthorSerializer, BookSerializer
+from rest_framework import permissions
 
-# Create your views here.
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAdminUser]    
+
+
+class CustomAuthToken(APIView):
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            token,created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
+        else:
+            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
